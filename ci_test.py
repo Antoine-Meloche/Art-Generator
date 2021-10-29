9f8d9100d69e31a96cbb1701735a9939e8b7adaf
Antoine Meloche <antmeloche@gmail.com>
2021-10-28 21:55:33 -0400

    Update ci_test.py

--- ci_test.py
+++ ci_test.py
@@ -8,11 +8,13 @@ def test_defaultGen():
     assert defaultGen.generate(
         2100, 1300, "#ffffff", "#242424", 50, 30, 10, 100, 10, 0, "./image.png") == 201
     end = time.time()
-    print(":\nThe Default algorithm took %ss to complete" % (end - start), end="")
+    print(":\nThe Default algorithm took %ss to complete" %
+          (end - start), end="")
 
 
 def test_pipesGen():
     start = time.time()
-    assert pipesGen.generate()
+    assert pipesGen.generate(2100, 1300, "#242424",
+                             "#ffffff", 3, .75, "./image.gif")
     end = time.time()
     print("\nThe Pipes algorithm took %ss to complete" % (end - start))
