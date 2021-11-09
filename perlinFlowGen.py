03eb8e8a0eb609245177301331a7dcaf66747fe7
Antoine Meloche <53838501+Antoine-Meloche@users.noreply.github.com>
2021-11-09 15:46:50 -0500

    Second pass on perlin flow generation

--- perlinFlowGen.py
+++ perlinFlowGen.py
@@ -14,13 +14,18 @@ def generate(width: int, height: int, fg: str, bkg: str, octaves: int, export_pa
 
 
 def noise(octaves: int, pixels, width: int, height: int):
+    set1 = []
+
     pts = []
     for x in range(octaves):
-        pts.append((x, random.random()))
+        rand_y = random.random()
+        pts.append((x, rand_y))
 
     pts = list(map(lambda pt: (map_nums(pt[0], octaves, width), map_nums(pt[1], 1, height)), pts))
 
-    for x in range(1,width+1):
+    x = 0
+    # for x in range(1,width+1):
+    while x <= width:
         y = 0
         for i in range(len(pts)):
             jresult = 1
@@ -29,6 +34,29 @@ def noise(octaves: int, pixels, width: int, height: int):
                     continue
                 jresult *= ((x-pts[j][0])/(pts[i][0]-pts[j][0]))
             y += pts[i][1]*jresult
+
+        if x % .5 == 0:
+            set1.append((x, y))
+
+        x += .5
+
+    x = 0
+    # for x in range(1,width+1):
+    while x <= width:
+        y = 0
+        for i in range(len(set1)):
+            jresult = 1
+            for j in range(len(set1)):
+                if i == j:
+                    continue
+                jresult *= ((x-set1[j][0])/(set1[i][0]-set1[j][0]))
+            y += set1[i][1]*jresult
+
+        set1[x*2][1] += y
+
+        x += .5
+
+    for (x, y) in set1:
         try:
             pixels[x, y] = (255, 255, 255)
         except IndexError:
