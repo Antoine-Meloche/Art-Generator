25149c99644894e004096c6e9dd9cf288177c2d6
Antoine Meloche <53838501+Antoine-Meloche@users.noreply.github.com>
2021-12-19 11:02:46 -0500

    Work from 15-18/12/2021

--- ci_test.py
+++ ci_test.py
@@ -1,6 +1,7 @@
 from time import time
 import defaultGen
 import perlinCircleGen
+import wavesGen
 
 
 def test_defaultGen():
@@ -18,4 +19,13 @@ def test_perlinCircleGen():
         1200, 1200, "#ffffff", "#242424", "./image.png") == 201
     end = time()
     print(":\nThe Perlin Circle algorithm took %ss to complete" %
+          (end - start), end="")
+
+
+def test_wavesGen():
+    start = time()
+    assert wavesGen.generate(2100, 1300, "#ffffff",
+                             "#242424", 10, 24, 2, "./waves.webp") == 201
+    end = time()
+    print(":\nThe Waves generation algorithm took %ss to complete" %
           (end - start), end="")
