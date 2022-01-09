b5c759a90f26df8d87ab1a7c73fe8f51b636b880
Antoine Meloche <53838501+Antoine-Meloche@users.noreply.github.com>
2022-01-09 07:40:52 -0500

    Modify ci_test to use perlinFlowGen instead of wavesGen

--- ci_test.py
+++ ci_test.py
@@ -1,6 +1,7 @@
 from time import time
 import defaultGen
 import perlinCircleGen
+import perlinFlowGen
 import wavesGen
 
 
@@ -9,8 +10,8 @@ def test_defaultGen():
     assert defaultGen.generate(
         2100, 1300, "#ffffff", "#242424", 50, 30, 10, 100, 10, 0, "./image.png") == 201
     end = time()
-    print(":\nThe Default algorithm took %ss to complete" %
-          (end - start), end="")
+    print("The Default algorithm took %ss to complete" %
+          (end - start))
 
 
 def test_perlinCircleGen():
@@ -18,14 +19,21 @@ def test_perlinCircleGen():
     assert perlinCircleGen.generate(
         1200, 1200, "#ffffff", "#242424", "./image.png") == 201
     end = time()
-    print(":\nThe Perlin Circle algorithm took %ss to complete" %
-          (end - start), end="")
+    print("The Perlin Circle algorithm took %ss to complete" %
+          (end - start))
 
 
-def test_wavesGen():
+def test_perlinFlowGen():
     start = time()
-    assert wavesGen.generate(2100, 1300, "#ffffff",
-                             "#242424", 10, 24, 2, "./waves.webp") == 201
+    assert perlinFlowGen.generate(2100, 1300, "#ffffff", "#242424", 50, 1000, 500, 1, "./image.png")
     end = time()
-    print(":\nThe Waves generation algorithm took %ss to complete" %
-          (end - start), end="")
+    print("The Perlin Flow algortihm took %ss to complete" % (end-start))
+
+
+# def test_wavesGen():
+#     start = time()
+#     assert wavesGen.generate(2100, 1300, "#ffffff",
+#                              "#242424", 10, 24, 2, "./waves.webp") == 201
+#     end = time()
+#     print(":\nThe Waves generation algorithm took %ss to complete" %
+#           (end - start), end="")
